4b76ac581c9784a5cc79fa2e4ee076e4980bca77
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-10-11 20:20:42 +0000

    Projecto Ej2

---
+++ trunk/Ejercicio 2/Ejercicio2Testers/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: knoppix
+ *
+ * Created on October 11, 2014, 1:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
