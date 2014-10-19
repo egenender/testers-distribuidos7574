ca7084ceefd223340052af99782f0a952206b667
mirian.quinteros.89875@gmail.com <mirian.quinteros.89875@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-10-19 15:30:37 +0000

    Comenzando ejercicio 5 de enserio

---
+++ trunk/Ejercicio 5/src/common/iPlanillaTesterA.cpp
@@ -0,0 +1,39 @@
+#include "iPlanillaTesterA.h"
+#include "common.h"
+#include <sys/ipc.h>
+
+iPlanillaTesterA::iPlanillaTesterA(int id) {
+    idTester = id;
+    key_t key = ftok(ipcFileName.c_str(), MSGQUEUE_PLANILLA);
+    this->cola = msgget(key, IPC_CREAT);
+}
+
+iPlanillaTesterA::iPlanillaTesterA(const iPlanillaTesterA& orig) {
+}
+
+iPlanillaTesterA::~iPlanillaTesterA() {
+}
+
+
+bool iPlanillaTesterA::agregar(int idDispositivo){
+    requerimiento_planilla_t requerimiento;
+    requerimiento.tester = idTester;
+    requerimiento.tipoReq = REQUERIMIENTO_AGREGAR;
+    requerimiento.idDispositivo = idDispositivo;
+    
+    msgsnd(cola,&requerimiento, sizeof(requerimiento_planilla_t) - sizeof(long),0);
+    
+    respuesta_lugar_t hayLugar;
+    msgrcv(cola, &hayLugar, sizeof(respuesta_lugar_t) - sizeof(long),idDispositivo ,0);
+    return hayLugar.respuesta;
+}
+
+void iPlanillaTesterA::terminoRequerimientoPendiente(){
+    requerimiento_planilla_t requerimiento;
+    requerimiento.tester = idTester;
+    requerimiento.tipoReq = REQUERIMIENTO_TERMINO_PENDIENTE_REQ;
+    requerimiento.idDispositivo = idDispositivo;
+    
+    msgsnd(cola,&requerimiento, sizeof(requerimiento_planilla_t) - sizeof(long),0);
+}
+
