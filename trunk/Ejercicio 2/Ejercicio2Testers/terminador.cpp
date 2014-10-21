f45fdf42ec123f88267803160b5de44fa36093a8
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-10-21 19:50:11 +0000

    Iniciador y Terminador bastante avanzados

--- trunk/Ejercicio 2/Ejercicio2Testers/terminador.cpp
+++ trunk/Ejercicio 2/Ejercicio2Testers/terminador.cpp
@@ -8,13 +8,14 @@
  */
 
 #include <cstdlib>
-#include <common/common.h>
-#include "unistd.h"
-
+#include "common/common.h"
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
 #include "logger/Logger.h"
-#include "common/AtendedorTesters.h"
-#include "common/Planilla.h"
-#include "common/DespachadorTecnicos.h"
+#include "ipc/Semaphore.h"
+#include "common/planilla_local.h"
 
 /*
  * 
@@ -28,33 +29,39 @@ int main(int argc, char** argv) {
     Logger::initialize(logFileName.c_str(), Logger::LOG_DEBUG);
     Logger::error("Logger inicializado. Destruyendo IPCs...", __FILE__);
    
-    // Creo objeto despachador para remover la cola de mensajes
-    DespachadorTecnicos despachador;
-    if (!despachador.destruirComunicacion()) {
-	Logger::error("No se pudo destruir la cola de mensajes del despachador...", __FILE__);
-    }
-
-    // Creo objeto atendedor para remover la cola de mensajes
-    AtendedorTesters atendedor;
-    if (!atendedor.destruirComunicacion()) {
-        Logger::error("No se pudo destruir la cola de mensajes del atendedor...", __FILE__);
-    }
-
-    // Creo objeto planilla para destruir el semaforo y la memoria compartida que utiliza
-    Planilla planilla;
-    if (!planilla.destruirMemoria()) {
-	Logger::error("No se pudo destruir la memoria compartida de la planilla...", __FILE__);
-    }
-    if (!planilla.destruirSemaforo()) {
-	std::string err = std::string("No se pudo destruir el semaforo de la planilla. Error: ") + std::string(strerror(errno));
-	Logger::error(err.c_str(), __FILE__);
-    }
-
-    unlink(ipcFileName.c_str());
- 
-    Logger::notice("IPCs eliminados...", __FILE__);
+	//Semaforo y Planilla General
+    key_t key = ftok(ipcFileName.c_str(), SHM_PLANILLA_GENERAL);
+    int shmgeneralid = shmget(key, sizeof(int), IPC_CREAT | 0660);
+    shmctl(shmgeneralid, IPC_RMID, NULL);
     
-    Logger::destroy();
+    Semaphore semPlanillaGeneral(SEM_PLANILLA_GENERAL);
+    semPlanillaGeneral.getSem();
+    semPlanillaGeneral.eliSem();
     
+    //Semaforos de Tester A y B, y planillas Locales
+    for (int i = 0; i < CANT_TESTERS; i++){
+        Semaphore semtesterA(SEM_TESTER_A + i);
+        Semaphore semtesterB(SEM_TESTER_B + i);
+        Semaphore semlocal(SEM_PLANILLA_LOCAL + i);
+        
+        semtesterA.getSem();
+        semtesterA.eliSem();
+        semtesterB.getSem();
+        semtesterB.eliSem();
+        semlocal.getSem();
+        semlocal.eliSem();
+        
+        key_t key = ftok(ipcFileName.c_str(), SHM_PLANILLA_LOCAL + i);
+        int shmlocalid = shmget(key, sizeof(planilla_local_t), IPC_CREAT | 0660);
+        shmctl(shmlocalid, IPC_RMID, NULL);
+    }
+    
+    //Destruccion de colas
+    for (int q = MSGQUEUE_PLANILLA; q <= MSGQUEUE_DESPACHADOR; q++){
+		key = ftok(ipcFileName.c_str(), q);
+		int cola = msgget(key, 0666 | IPC_CREAT);
+		msgctl(cola ,IPC_RMID, NULL);
+	}
+       
     return 0;
 }
