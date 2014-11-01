fcbdb321d57f9bd4ea4da70abc22ee8a626af5d4
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-01 16:36:08 +0000

    Eliminada la carpeta de cosas comunes, que estaba mas al pedo que bocina de avion

--- trunk/Cosas Comunes/Programa.cpp
+++
@@ -1,8 +0,0 @@
-#include "Programa.h"
-#include <cstdlib>
-
-program_t Programa::getPrograma() const{
-	program_t programa;
-	programa.tiempo = rand() % (MAXIMO_TIEMPO +1) + MINIMO_TIEMPO;
-	programa.prob = rand() % 100;
-}
