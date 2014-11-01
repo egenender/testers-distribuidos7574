09d83de3beb09b139e8617b198b3fe66b9e88dad
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-01 18:03:30 +0000

    Copiado ejercicio 6 al 7, falta determinar si los testers especiales deben quedarse colgados o no esperando si hay errores o no

---
+++ Ejercicio 7/Implementacion/common/common.h
@@ -0,0 +1,75 @@
+/* 
+ * File:   common.h
+ * Author: knoppix
+ *
+ * Created on October 4, 2014, 7:13 PM
+ * 
+ * Aqui se almacenan las constantes correspondientes a toda la app, incluido nombre de archivos para IPCs
+ */
+
+#include <string>
+#include <stddef.h>
+
+#ifndef COMMON_H
+#define	COMMON_H
+
+// Constantes del sistema
+
+const int CANT_DISPOSITIVOS = 100;
+const int CANT_TESTERS_COMUNES = 3;
+const int CANT_TESTERS_ESPECIALES = 5;
+const int ID_DISPOSITIVO_START = 50;
+const int ID_TESTER_START = 2;
+const int MAX_DISPOSITIVOS_EN_SISTEMA = 100;
+
+
+// IDs de los IPC
+const int SEM_PLANILLA_GENERAL = 1;
+const int SHM_PLANILLA_GENERAL = 2;
+
+const int MSGQUEUE_DISPOSITIVOS = 10;
+const int MSGQUEUE_TESTERS = 11;
+const int MSGQUEUE_DESPACHADOR = 12;
+
+const int MTYPE_REQUERIMIENTO = 1;
+
+const int ORDEN_APAGADO = 0;
+const int ORDEN_REINICIO = 1;
+const int ORDEN_SEGUIR_TESTEANDO = 2;
+const int SIN_LUGAR = -1;
+
+const int CANT_RESULTADOS = 100;
+const int RESULTADO_GRAVE = 0;
+const int RESULTADO_NO_GRAVE = 1;
+const int SEGUIR_TESTEANDO = 2;
+const int NO_CONTESTAR = 3;
+
+const int MAXIMO_TESTERS_ESPECIALES_POR_ESPECIFICACION = 4;
+const int MINIMO_TESTERS_ESPECIALES_POR_ESPECIFICACION = 2;
+// Archivos necesarios
+
+const std::string ipcFileName = "tmp/buchwaldipcs";
+
+const std::string logFileName = "log.txt";
+
+
+//Estructuras communes:
+typedef struct resultado{
+	int idDispositivo;
+	int resultadosPendientes;
+	int resultadosGraves;
+}resultado_t;
+
+typedef struct posicion_en_shm{
+	long mtype;
+	int lugar;
+}posicion_en_shm_t;
+
+typedef struct resultado_test{
+    long tester;
+    int result;
+    int dispositivo;
+}resultado_test_t;
+
+#endif	/* COMMON_H */
+
