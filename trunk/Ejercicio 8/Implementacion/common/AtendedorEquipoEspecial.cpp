785ef951eb745fc719ceab0c5559b52f7491d00b
fer.pereira9014@gmail.com <fer.pereira9014@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-18 04:13:48 +0000

    Se agrega implementacion del ejercicio 8

---
+++ trunk/Ejercicio 8/Implementacion/common/AtendedorEquipoEspecial.cpp
@@ -0,0 +1,79 @@
+/* 
+ * File:   AtendedorEquipoEspecial.cpp
+ * Author: ferno
+ * 
+ * Created on November 17, 2014, 12:59 AM
+ */
+
+#include "AtendedorEquipoEspecial.h"
+
+AtendedorEquipoEspecial::AtendedorEquipoEspecial() {
+    key_t key = ftok(ipcFileName.c_str(), MSGQUEUE_DISPOSITIVOS_TESTERS_ESPECIALES);
+    this->colaDispTesterEsp = msgget(key, 0666);
+    if(this->colaDispTesterEsp == -1) {
+        std::string err = std::string("Error al obtener la cola para enviar fin de tareas especiales a los dispositivos. Errno: ") + std::string(strerror(errno));
+        Logger::error(err, __FILE__);
+        exit(1);
+    }
+    
+    key = ftok(ipcFileName.c_str(), MSGQUEUE_REINICIO_TESTEO);
+    this->colaReinicioTestEsp = msgget(key, 0666);
+    if(this->colaReinicioTestEsp == -1) {
+        std::string err = std::string("Error al obtener la cola para avisar reinicio de testeo especial. Errno: ") + std::string(strerror(errno));
+        Logger::error(err, __FILE__);
+        exit(1);
+    }
+    
+    key = ftok(ipcFileName.c_str(), MSGQUEUE_DISPOSITIVOS);
+    this->colaOrdenDispositivos = msgget(key, 0666);
+    if(this->colaOrdenDispositivos == -1) {
+        std::string err = std::string("Error al obtener la cola para enviar orden de apagado o reinicio a dispositivos. Errno: ") + std::string(strerror(errno));
+        Logger::error(err, __FILE__);
+        exit(1);
+    }
+}
+
+AtendedorEquipoEspecial::~AtendedorEquipoEspecial() {
+}
+
+TResultadoEspecial AtendedorEquipoEspecial::recibirResultadoEspecial() {
+    TResultadoEspecial resultado;
+    int ret = msgrcv(this->colaDispTesterEsp, &resultado, sizeof(TResultadoEspecial) - sizeof(long), MTYPE_RESULTADO_ESPECIAL, 0);
+    if(ret == -1) {
+        std::string error = std::string("Error al recibir resultado especial de algun dispositivo. Error: ") + std::string(strerror(errno));
+        Logger::error(error.c_str(), __FILE__);
+        exit(0);
+    }
+    return resultado;
+}
+
+void AtendedorEquipoEspecial::enviarFinTestEspecialADispositivo(int idDispositivo) {
+    TMessageAtendedor msg;
+    msg.mtype = idDispositivo;
+    msg.idDispositivo = idDispositivo;
+    msg.value = FIN_TEST_ESPECIAL;
+    int ret = msgsnd(this->colaDispTesterEsp, &msg, sizeof(TMessageAtendedor) - sizeof(long), 0);
+    if(ret == -1) {
+        std::ostringstream ss;
+        ss << "Error al enviar fin de testeo especial al dispositivo " << idDispositivo << ". Error: ";
+        std::string error = ss.str() + std::string(strerror(errno));
+        Logger::error(error.c_str(), __FILE__);
+        exit(0);
+    }
+}
+
+void AtendedorEquipoEspecial::enviarOrden(int idDispositivo, int orden) {
+    TMessageAtendedor msg;
+    msg.mtype = idDispositivo;
+    msg.idDispositivo = idDispositivo;
+    msg.idTester = -1;
+    msg.value = orden;
+    int ret = msgsnd(this->colaOrdenDispositivos, &msg, sizeof(TMessageAtendedor) - sizeof(long), 0);
+    if(ret == -1) {
+        std::ostringstream ss;
+        ss << "Error al enviar orden de apagado o reinicio al dispositivo " << idDispositivo << ". Error: ";
+        std::string error = ss.str() + std::string(strerror(errno));
+        Logger::error(error.c_str(), __FILE__);
+        exit(0);
+    }
+}
---
+++ trunk/Ejercicio 8/Implementacion/common/Planilla.cpp
@@ -0,0 +1,94 @@
+/* 
+ * File:   Planilla.cpp
+ * Author: knoppix
+ * 
+ * Created on October 4, 2014, 10:08 PM
+ */
+
+#include "Planilla.h"
+#include "logger/Logger.h"
+
+Planilla::Planilla() : semShMem(SEM_PLANILLA_GENERAL) {
+    
+    this->shMemKey = ftok(ipcFileName.c_str(), SHM_PLANILLA_GENERAL);
+    if(this->shMemKey == -1) {
+        std::string err("Error al conseguir la key de la shmem de la planilla general. Error: " + std::string(strerror(errno)));
+        Logger::error(err.c_str(), __FILE__);
+        throw err;
+    }
+    this->shMemId = shmget(this->shMemKey, sizeof(int), IPC_CREAT | 0660);
+    if(this->shMemId == -1) {
+        std::string err("Error al conseguir la memoria compartida de la planilla general. Error: " + std::string(strerror(errno)));
+        Logger::error(err.c_str(), __FILE__);
+        throw err;
+    }
+    
+    void* tmpPtr = shmat (this->shMemId , NULL ,0);
+    if ( tmpPtr != (void*) -1 ) {
+        this->cantDispositivosSiendoTesteados = static_cast<int*> (tmpPtr);
+        Logger::debug("Memoria compartida de la planilla creada correctamente", __FILE__);
+    } else {
+        std::string err = std::string("Error en shmat() de planilla general. Error: ") + std::string(strerror(errno));
+        Logger::error(err, __FILE__);
+        throw err;
+    }
+
+    // Por ultimo, luego de creado, obtengo el semaforo correspondiente
+    if (!this->semShMem.getSem()) {
+	std::string err = std::string("Error al obtener el semaforo de la planilla general. Error: ") + std::string(strerror(errno));
+	Logger::error(err, __FILE__);
+	throw err;
+    }
+
+}
+
+Planilla::Planilla(const Planilla& orig) : semShMem(SEM_PLANILLA_GENERAL) {
+}
+
+void Planilla::initPlanilla() {
+    this->semShMem.p();
+    *(this->cantDispositivosSiendoTesteados) = 0;
+    this->semShMem.v();
+}
+
+Planilla::~Planilla() {
+}
+
+bool Planilla::hayLugar() {
+
+    // Si hay lugar, registro el dispositivo
+    bool result = true;
+    this->semShMem.p();
+    if(*this->cantDispositivosSiendoTesteados >= MAX_DISPOSITIVOS_EN_SISTEMA) {
+        result = false;
+    } else {
+        *this->cantDispositivosSiendoTesteados += 1;
+    }
+    this->semShMem.v();
+    return result;
+}
+void Planilla::eliminarDispositivo() {
+
+    this->semShMem.p();
+    *this->cantDispositivosSiendoTesteados -= 1;
+    this->semShMem.v();
+}
+
+int Planilla::cantProcesosUsandoPlanilla() {
+    shmid_ds estado;
+    if (shmctl(this->shMemId, IPC_STAT, &estado) == -1) {
+        std::string err = std::string("Error en shmctl() al verificar la cantidad de procesos adosados a la planilla. Error: ") + std::string(strerror(errno));
+        Logger::error(err, __FILE__);
+    }
+	return estado.shm_nattch;
+}
+
+bool Planilla::destruirMemoria() {
+
+    return (shmctl(this->shMemId, IPC_RMID, NULL) != -1);
+}
+
+bool Planilla::destruirSemaforo() {
+
+    return (this->semShMem.eliSem());
+}
