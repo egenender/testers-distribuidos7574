1f8e0b93449697c3b68ad0f3bb741481e81ca08d
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-24 17:36:29 +0000

    Dos aplicaciones de utilizadores del ejemplo, para probarlo en dos computadoras distintas

---
+++ trunk/tcp/codigo/tcp-recepcion/utilizador_client.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+
+
+typedef struct mensaje{
+	long mtype;
+	char buffer[10];
+} mensaje_t;
+
+
+int main(void){
+
+	key_t key = ftok("ipcs-prueba", 2);
+	int cola_client = msgget(key, 0660 | IPC_CREAT | IPC_EXCL);
+	
+	mensaje_t mensaje;
+	printf("Tam del mensaje: %zu\n", sizeof(mensaje_t));
+	
+	mensaje_t otro_mensaje;
+	
+	int ok_read = msgrcv(cola_client, &otro_mensaje, sizeof(mensaje_t) - sizeof(long), 1, 0);
+    if (ok_read == -1){
+		exit(0);
+	}
+	
+	printf("El mensaje recibido fue %s\n", otro_mensaje.buffer);
+}
