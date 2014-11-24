f25f1426c2f7c279504e48d194fbb4021b40d545
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-24 15:48:33 +0000

    Tcp de prueba sacando y poniendo en colas

--- tcp/codigo/tcp-recepcion/tcpclient.cpp
+++ tcp/codigo/tcp-recepcion/tcpclient.cpp
@@ -1,6 +1,7 @@
 #include "../tcp.h"
 #include <errno.h>
 #include <stdio.h>
+#include <sys/msg.h>
 #include "../measure.h"
 
 /**
@@ -8,21 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-    char buffer[4192];
-
-    if(argc != 4){
-        printf("%s <host> <port> <tamaño de recepción>\n",argv[0]);
+        if(argc != 5){
+        printf("%s <host> <port> <id_cola> <tam mensaje>\n",argv[0]);
         return -1;
     }
 
-    int size = atoi(argv[3]);
-    if(size<=0 || size>4192){
-      printf("Error: el tamaño debe estar entre 1 y 4192\n");
-      return -1;
-    }
-    
+    int size = atoi(argv[4]);
+    int id_cola = atoi(argv[3]);
+        
     int fd = tcp_open_activo(argv[1], atoi(argv[2]));
-    if(fd<0){
+    if(fd < 0){
       perror("Error");
       return -2;
     }
@@ -31,17 +27,29 @@ int main(int argc, char *argv[])
     getchar();
 
     tic();
-    int leido=0, acumulado=0, lecturas=0;
-    while( (leido = read(fd, buffer, size)) > 0){
+    int leido = 0;
+    size_t acumulado = 0;
+    void* buffer = malloc(size);
+    /*while( (leido = read(fd, buffer, size)) > 0){
         lecturas++;
         acumulado += leido;
-        printf("%s\n", buffer);
+    }*/
+    char* buffer_act = (char*) buffer;
+    while( (leido = read(fd, buffer_act, size - acumulado)) > 0){
+        acumulado += leido;
+        buffer_act = buffer_act + leido;
     }
+    
     double time_ms=toc();
 
     printf("Tiempo total: %f ms.\n", time_ms);
 
-    printf("Recibidos %i bytes en %i lecturas\n", acumulado, lecturas);
+	key_t key = ftok("ipcs-prueba", id_cola);
+    int cola = msgget(key, 0660);
+    int ok = msgsnd(cola, buffer, size - sizeof(long), 0);
+    if (ok == -1){
+		exit(0);
+	}	
 
     return 0;
 }
