a2185eb63b602b9aeb786954cce2860150cc629a
dario.fenix@gmail.com <dario.fenix@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2014-11-25 19:21:17 +0000

    Separo implementaciones centralizada y distribuida

---
+++ Ejercicio 9/centralizado/src/TesterEspecial.cpp
@@ -0,0 +1,82 @@
+/* 
+ * File:   Tester.cpp
+ * Author: knoppix
+ *
+ * Created on October 4, 2014, 8:46 PM
+ */
+
+#include <cstdlib>
+#include "common/AtendedorTesters.h"
+#include "common/Programa.h"
+#include "common/Resultado.h"
+#include "common/DespachadorTesters.h"
+#include "common/Planilla.h"
+#include "common/PlanillaAsignacionTesterEspecial.h"
+#include "common/PlanillaReinicioTesterEspecial.h"
+#include "logger/Logger.h"
+#include "common/TareaEspecial.h"
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    // El primer parametro es el id del tester
+    int id = atoi(argv[1]);
+    Logger::initialize(logFileName.c_str(), Logger::LOG_DEBUG);
+    std::stringstream nombre;
+    nombre << __FILE__ << " " << id;
+    Logger::notice("Inicia el procesamiento, cargo el atendedor y despachador" , nombre.str().c_str());
+
+    // Obtengo comunicacion con los dispositivos
+    AtendedorTesters atendedor;
+    // Obtengo comunicacion con los tecnicos
+    DespachadorTesters despachador;
+    // Obtengo planilla general de sync con otros tester
+    Planilla planilla;
+    PlanillaAsignacionTesterEspecial planillaAsignacion;
+    PlanillaReinicioTesterEspecial planillaReinicio;
+
+    srand(time(NULL));
+
+    while(true) {
+        Logger::notice("Espero por un nuevo requerimiento de testeo especial" , nombre.str().c_str());
+        // Espero un requerimiento
+        TMessageAssignTE msg = atendedor.recibirRequerimientoEspecial(id);
+        stringstream ss;
+        ss << msg.idDispositivo;
+        string mensaje = "Recibido requerimiento desde dispositivo id ";
+        Logger::notice(mensaje + ss.str() , nombre.str().c_str());
+        ss.clear();
+        ss.str("");
+        ss << "Enviando pedido de test de configuracion para dispositivo con id " << msg.idDispositivo;
+        Logger::notice(ss.str() , nombre.str().c_str());
+        ss.str("");
+        atendedor.enviarReqTestConfig( id, msg.idDispositivo, msg.tipoDispositivo );
+        usleep( rand() % 1000 + 1000);
+
+        TareaEspecial tarea;
+        tarea.prepararTareasEspeciales();
+
+        do {
+            tarea.resetTareas();
+            planillaAsignacion.asignarCantTareasEspeciales(msg.posicionDispositivo, tarea.getCantTareasEspeciales());
+            for (int i = 0; i < tarea.getCantTareasEspeciales(); i++) {
+                int tareaActual = tarea.getProximaTareaEspecial();
+                ss << "Envio programa de testeo especial " << tareaActual << " al dispositivo " << msg.idDispositivo;
+                Logger::notice(ss.str() , nombre.str().c_str());
+                ss.str("");
+                if (tareaActual == -1) break;
+                atendedor.enviarTareaEspecial(msg.idDispositivo, id, tareaActual, msg.posicionDispositivo);
+            }
+
+            ss << "Aviso que termine de enviar las " << tarea.getCantTareasEspeciales() << " tareas especiales";
+            Logger::notice(ss.str() , nombre.str().c_str());
+            ss.str("");
+            planillaAsignacion.avisarFinEnvioTareas(msg.posicionDispositivo);
+
+            usleep( rand() % 1000 + 1000);
+        } while (planillaReinicio.hayQueReiniciar(id));
+    }
+
+    return 0;
+}
---
+++ Ejercicio 9/centralizado/src/finalizador.cpp
@@ -0,0 +1,68 @@
+/* 
+ * File:   terminador.cpp
+ * Author: knoppix
+ *
+ * Created on October 4, 2014, 7:09 PM
+ * 
+ * Destruye todos los IPCs utilizados en la aplicacion
+ */
+
+#include <cstdlib>
+#include "common/common.h"
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include "logger/Logger.h"
+#include "ipc/Semaphore.h"
+#include "common/Planilla.h"
+#include "common/PlanillaAsignacionEquipoEspecial.h"
+
+
+int main(int argc, char** argv) {
+    
+    Logger::initialize(logFileName.c_str(), Logger::LOG_DEBUG);
+    Logger::error("Logger inicializado. Destruyendo IPCs...", __FILE__);
+   
+    //Semaforo y Planilla General
+    key_t key = ftok(ipcFileName.c_str(), SHM_PLANILLA_GENERAL);
+    int shmgeneralid = shmget(key, sizeof(resultado_t) * CANT_RESULTADOS,  0660);
+    shmctl(shmgeneralid, IPC_RMID, NULL);
+    
+    Planilla planillaGeneral;
+    if (!planillaGeneral.destruirMemoria()) {
+        Logger::warn("No se pudo destruir la memoria compartida de la planilla general", __FILE__);
+    }
+    
+    PlanillaAsignacionEquipoEspecial planillaAsignacion;
+    if (!planillaAsignacion.destruirComunicacion()) {
+        Logger::warn("No se pudo destruir alguna memoria compartida de la planilla de asignacion", __FILE__);
+    }
+    
+    Semaphore semPlanillaGeneral(SEM_PLANILLA_GENERAL);
+    semPlanillaGeneral.getSem();
+    semPlanillaGeneral.eliSem();
+		
+    Semaphore sem_cola_especiales(SEM_COLA_ESPECIALES);
+    sem_cola_especiales.getSem();
+    sem_cola_especiales.eliSem();
+    
+    Semaphore semPlanillaCantTestersAsignados(SEM_PLANILLA_CANT_TESTER_ASIGNADOS);
+    semPlanillaCantTestersAsignados.getSem();
+    semPlanillaCantTestersAsignados.eliSem();
+    
+    Semaphore semPlanillaCantTareasAsignadas(SEM_PLANILLA_CANT_TAREAS_ASIGNADAS);
+    semPlanillaCantTareasAsignadas.getSem();
+    semPlanillaCantTareasAsignadas.eliSem();
+	
+    //Destruccion de colas
+    for (int q = MSGQUEUE_DISPOSITIVOS; q <= MSGQUEUE_ULTIMO; q++){
+        key = ftok(ipcFileName.c_str(), q);
+        int cola = msgget(key, 0660);
+        msgctl(cola ,IPC_RMID, NULL);
+    }
+    
+    unlink(ipcFileName.c_str());
+    
+    return 0;
+}
