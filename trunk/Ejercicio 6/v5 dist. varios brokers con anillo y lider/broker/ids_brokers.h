075f1cc8f5a016e7c3e95500153481bca7fd04d6
martine9111@gmail.com <martine9111@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2015-01-24 18:27:45 +0000

    Formateado el id_brokers.h para que matchee con el setup y su regex

--- trunk/Ejercicio 6/v5 dist. varios brokers con anillo y lider/broker/ids_brokers.h
+++ trunk/Ejercicio 6/v5 dist. varios brokers con anillo y lider/broker/ids_brokers.h
@@ -4,10 +4,6 @@ typedef struct broker_id_ip{
 	char ip[16];
 }broker_id_ip_t;
 
-broker_id_ip_t BROKERS[] = { 
-	{1001, "192.168.1.104"},
-	{1002, "192.168.1.101"},
-	{1003, "192.168.1.108"}
-};
+broker_id_ip_t BROKERS[] = {{1001, "192.168.1.104"},{1002, "192.168.1.101"},{1003, "192.168.1.108"}};
 
 #define CANT_BROKERS 3
