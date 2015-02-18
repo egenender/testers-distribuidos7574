aac728b4f46d4e2a0d4146be4108166da3a21382
fer.pereira9014@gmail.com <fer.pereira9014@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2015-02-18 04:12:45 +0000

    Para ver si modifica bien la shmem de lider

--- trunk/Ejercicio 8/Iteracion4-varios-brokers-anillo/broker/brokersInfo.h
+++ trunk/Ejercicio 8/Iteracion4-varios-brokers-anillo/broker/brokersInfo.h
@@ -12,6 +12,6 @@ typedef struct TBrokerInfo {
     char* ipBroker;
 } TBrokerInfo;
 
+//const TBrokerInfo IP_BROKERS[CANT_BROKERS] = {{1, "127.0.0.1"}, {2, "192.168.2.3"}, {3, "192.168.2.9"}};
 const TBrokerInfo IP_BROKERS[CANT_BROKERS] = {{1, "127.0.0.1"}, {2, "192.168.2.3"}};
-
 #endif	/* BROKERSINFO_H */
