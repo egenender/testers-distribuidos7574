2b2954affe49aea31b67236be3bde5bf9f0441db
dario.fenix@gmail.com <dario.fenix@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2015-03-01 01:03:47 +0000

    Errores en rojo en el log

--- Ejercicio 9/centralizado/src/logger/Logger.cpp
+++ Ejercicio 9/centralizado/src/logger/Logger.cpp
@@ -8,43 +8,51 @@
 #include "Logger.h"
 
 Logger* Logger::_logger = NULL;
+const std::string Logger::INICIO_HEADER_COLOR_VT100 = "\x1b[";
+const std::string Logger::FIN_HEADER_COLOR_VT100 = "m";
+const std::string Logger::FOOTER_COLOR_VT100 = "\x1b[0m\n";
+const int Logger::VT100_BLANCO = 37;
+const int Logger::VT100_ROJO = 31;
 
 void Logger::log(const std::string& msg, unsigned short int logLevel) {
-	if (logLevel >= _logger->_logLevel) {
-		time_t timer;
-		time(&timer);
-		_logger->_currentTime = localtime(&timer);
-		std::ostringstream oss;
-		oss << std::setfill('0')
-			<< std::setw(2) << _logger->_currentTime->tm_mday << "/"
-			<< std::setw(2) << _logger->_currentTime->tm_mon + 1 << "/"
-			<< _logger->_currentTime->tm_year + 1900 << " "
-			<< std::setw(2) << _logger->_currentTime->tm_hour << ":"
-			<< std::setw(2) << _logger->_currentTime->tm_min << ":"
-			<< std::setw(2) << _logger->_currentTime->tm_sec << " "
-			<< msg << "\n";
-		_logger->_output.tomarLock();
-		_logger->_output.escribir((const void*)oss.str().c_str(), (long int)oss.str().size());
-		// Comentar esto cuando haga falta
-		write(1, (const void*)oss.str().c_str(), oss.str().size());
-		_logger->_output.liberarLock();
-	}
+    if (logLevel >= _logger->_logLevel) {
+        time_t timer;
+        time(&timer);
+        _logger->_currentTime = localtime(&timer);
+        int color = logLevel == LOG_CRITICAL? VT100_ROJO : VT100_BLANCO;
+        std::ostringstream oss;        
+        oss << INICIO_HEADER_COLOR_VT100 << color << FIN_HEADER_COLOR_VT100
+            << std::setfill('0')
+            << std::setw(2) << _logger->_currentTime->tm_mday << "/"
+            << std::setw(2) << _logger->_currentTime->tm_mon + 1 << "/"
+            << _logger->_currentTime->tm_year + 1900 << " "
+            << std::setw(2) << _logger->_currentTime->tm_hour << ":"
+            << std::setw(2) << _logger->_currentTime->tm_min << ":"
+            << std::setw(2) << _logger->_currentTime->tm_sec << " "
+            << msg << "\n"
+            << FOOTER_COLOR_VT100;
+        _logger->_output.tomarLock();
+        _logger->_output.escribir((const void*)oss.str().c_str(), (long int)oss.str().size());
+        // Comentar esto cuando haga falta
+        write(1, (const void*)oss.str().c_str(), oss.str().size());
+        _logger->_output.liberarLock();
+    }
 }
 
 std::string Logger::prependCaller(const std::string& msg, const std::string& caller) {
-	return "[ " + caller + " ] " + msg;
+    return "[ " + caller + " ] " + msg;
 }
 
 void Logger::log(const std::string& msg, unsigned short int logLevel, const std::string& caller) {
-	log( prependCaller(msg, caller), LOG_DEBUG );
+    log( prependCaller(msg, caller), LOG_DEBUG );
 }
 
 void Logger::debug(const std::string& msg) {
-	log( msg, LOG_DEBUG );
+    log( msg, LOG_DEBUG );
 }
 
 void Logger::debug(const std::string& msg, const std::string& caller) {
-	log( msg, LOG_NOTICE, caller );
+    log( msg, LOG_NOTICE, caller );
 }
 
 void Logger::notice(const std::string& msg) {
@@ -52,22 +60,22 @@ void Logger::notice(const std::string& msg) {
 }
 
 void Logger::notice(const std::string& msg, const std::string& caller) {
-	log( msg, LOG_WARNING, caller );
+    log( msg, LOG_WARNING, caller );
 }
 
 void Logger::warn(const std::string& msg) {
-	log( msg, LOG_WARNING );
+    log( msg, LOG_WARNING );
 }
 
 void Logger::warn(const std::string& msg, const std::string& caller) {
-	log( msg, LOG_WARNING, caller );
+    log( msg, LOG_WARNING, caller );
 }
 
 void Logger::error(const std::string& msg) {
-	log( msg, LOG_CRITICAL );
+    log( msg, LOG_CRITICAL );
 }
 
 void Logger::error(const std::string& msg, const std::string& caller) {
-	log( msg, LOG_CRITICAL, caller );
+    log( msg, LOG_CRITICAL, caller );
 }
 
--- Ejercicio 9/centralizado/src/logger/Logger.h
+++ Ejercicio 9/centralizado/src/logger/Logger.h
@@ -5,8 +5,7 @@
  * Created on October 4, 2014, 7:24 PM
  */
 
-#ifndef LOGGER_H
-#define	LOGGER_H
+#pragma once
 
 #include <iostream>
 #include <fstream>
@@ -18,72 +17,75 @@
 class Logger {
 
 private:
-	LockFile _output;
-	unsigned short int _logLevel;
-	struct tm* _currentTime;
-	static Logger* _logger;
+    LockFile _output;
+    unsigned short int _logLevel;
+    struct tm* _currentTime;
+    static Logger* _logger;
+    static const int VT100_BLANCO;
+    static const int VT100_ROJO;
+    static const std::string INICIO_HEADER_COLOR_VT100;
+    static const std::string FIN_HEADER_COLOR_VT100;
+    static const std::string FOOTER_COLOR_VT100;
 
-	Logger(const char* filename) : _output(filename), _logLevel(Logger::LOG_NOTICE), _currentTime(NULL) {};
-	virtual ~Logger() {
-		//delete _output;
-	};
+    Logger(const char* filename) : _output(filename), _logLevel(Logger::LOG_NOTICE), _currentTime(NULL) {};
+    virtual ~Logger() {
+        //delete _output;
+    };
 
-	static std::string prependCaller(const std::string& msg, const std::string& caller);
+    static std::string prependCaller(const std::string& msg, const std::string& caller);
 
 public:
-	// TODO: ¿Algun level mas? Creo que son suficientes
-	static const unsigned short int LOG_DEBUG = 1;
-	static const unsigned short int LOG_NOTICE = 2;
-	static const unsigned short int LOG_WARNING = 3;
-	static const unsigned short int LOG_CRITICAL = 4;
+    // TODO: ¿Algun level mas? Creo que son suficientes
+    static const unsigned short int LOG_DEBUG = 1;
+    static const unsigned short int LOG_NOTICE = 2;
+    static const unsigned short int LOG_WARNING = 3;
+    static const unsigned short int LOG_CRITICAL = 4;
 
-	/*
-	 * filename: nombre del archivo a crear
-	 * logLevel: representa uno de los 4 niveles soportados por la clase
-	 * */
-	static void initialize(const char* filename, unsigned short int logLevel) {
-		// TODO: Ptr Error check
-		if (!_logger) {
-			_logger = new Logger(filename);
-			_logger->_logLevel = logLevel;
-		}
-	};
+    /*
+     * filename: nombre del archivo a crear
+     * logLevel: representa uno de los 4 niveles soportados por la clase
+     * */
+    static void initialize(const char* filename, unsigned short int logLevel) {
+        // TODO: Ptr Error check
+        if (!_logger) {
+            _logger = new Logger(filename);
+            _logger->_logLevel = logLevel;
+        }
+    };
 
-	static void destroy() {
-		if (_logger)
-			delete _logger;
-	}
+    static void destroy() {
+        if (_logger)
+            delete _logger;
+    }
 
-	static void setLogLevel( unsigned short int logLevel) {
-		_logger->_logLevel = logLevel;
-	}
+    static void setLogLevel( unsigned short int logLevel) {
+        _logger->_logLevel = logLevel;
+    }
 
-	/*
-	 * Loggea mensaje si y solo si el logLevel es mayor al setteado en la inicializacion
-	 * */
-	static void log(const std::string& msg, unsigned short int logLevel);
+    /*
+     * Loggea mensaje si y solo si el logLevel es mayor al setteado en la inicializacion
+     * */
+    static void log(const std::string& msg, unsigned short int logLevel);
 
-	/*
-	 * Loggea mensaje si y solo si el logLevel es mayor al setteado en la inicializacion
-	 * Prefija el mensaje con el nombre de quien loguea
-	 * */
-	static void log(const std::string& msg, unsigned short int logLevel, const std::string& caller);
+    /*
+     * Loggea mensaje si y solo si el logLevel es mayor al setteado en la inicializacion
+     * Prefija el mensaje con el nombre de quien loguea
+     * */
+    static void log(const std::string& msg, unsigned short int logLevel, const std::string& caller);
 
-	/*
-	 * Las siguientes funciones loguean el mensaje sólo si el logLevel seteado en la inicialización
-	 * es mayor a lo que indica el nombre de la función.
-	 * El segundo parámetro es prefijado ante el mensaje, si aparece
-	 * */
-	static void debug(const std::string& msg);
-	static void debug(const std::string& msg, const std::string& caller);
-	static void notice(const std::string& msg);
-	static void notice(const std::string& msg, const std::string& caller);
-	static void warn(const std::string& msg);
-	static void warn(const std::string& msg, const std::string& caller);
-	static void error(const std::string& msg);
-	static void error(const std::string& msg, const std::string& caller);
+    /*
+     * Las siguientes funciones loguean el mensaje sólo si el logLevel seteado en la inicialización
+     * es mayor a lo que indica el nombre de la función.
+     * El segundo parámetro es prefijado ante el mensaje, si aparece
+     * */
+    static void debug(const std::string& msg);
+    static void debug(const std::string& msg, const std::string& caller);
+    static void notice(const std::string& msg);
+    static void notice(const std::string& msg, const std::string& caller);
+    static void warn(const std::string& msg);
+    static void warn(const std::string& msg, const std::string& caller);
+    static void error(const std::string& msg);
+    static void error(const std::string& msg, const std::string& caller);
 
 };
-
-#endif	/* LOGGER_H */
 
