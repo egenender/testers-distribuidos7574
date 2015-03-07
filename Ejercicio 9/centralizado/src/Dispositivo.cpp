e2ddac50d1d535cd82279dd58c0e978d208d794e
dario.fenix@gmail.com <dario.fenix@gmail.com@18dcfaaf-94c9-9b71-9433-3f00c941b258>
2015-03-07 23:21:19 +0000

    Faltaba el id de Dispositivo y de DispositivoConfig en los mensajes de log

--- Ejercicio 9/centralizado/src/Dispositivo.cpp
+++ Ejercicio 9/centralizado/src/Dispositivo.cpp
@@ -45,14 +45,17 @@ int main(int argc, char** argv) {
     int id = atoi(argv[1]);
     int tipo = atoi(argv[2]);
 
+    std::stringstream nombreProceso;
+    nombreProceso << __FILE__ << " " << id;
+
     std::stringstream ss;
     ss << "Dispositivo " << id << " de tipo " << tipo << " creado";
-    Logger::debug(ss.str().c_str(), __FILE__);
+    Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
     ss.str("");
     
     Configuracion config;
     if( !config.LeerDeArchivo() ){
-        Logger::error("Archivo de configuracion no encontrado", __FILE__);
+        Logger::error("Archivo de configuracion no encontrado", nombreProceso.str().c_str());
         return 1;
     }
     
@@ -66,14 +69,14 @@ int main(int argc, char** argv) {
     try {
         // Dispositivo envia requerimiento
         ss << "El dispositivo " << id << " envia un requerimiento al sistema de testeo";
-        Logger::debug(ss.str().c_str(), __FILE__);
+        Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
         ss.str("");
         atendedor.enviarRequerimiento(id, tipo);
         // Recibe programa, verificando que no sea un rechazo por parte del sistema
         int program = atendedor.recibirPrograma(id);
         if (program == SIN_LUGAR) {
             ss << "El dispositivo " << id << " recibe indicacion de que no hay lugar en el sistema de testeo. Reintentara luego";
-            Logger::debug(ss.str().c_str(), __FILE__);
+            Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
             ss.str("");
             // Si no hay programa -> no hay lugar -> Duermo y envio otro req mas tarde
             usleep(rand() % 10000 + 10000);
@@ -81,19 +84,19 @@ int main(int argc, char** argv) {
         }
     
         ss << "El dispositivo " << id << " recibe el programa numero " << program;
-        Logger::debug(ss.str().c_str(), __FILE__);
+        Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
         ss.str("");
 
         int resul = correrTest( program );
         
         // Le envio resultado del primer programa de testeo        
         ss << "El dispositivo " << id << " envia los resultados";
-        Logger::debug(ss.str().c_str(), __FILE__);
+        Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
         ss.str("");
         atendedor.enviarResultado( id, resul );
 
         ss << "El dispositivo " << id << " espera la orden del sistema de testeo...";
-        Logger::debug(ss.str().c_str(), __FILE__);
+        Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
         ss.str("");
 
         // Recibo la orden a seguir
@@ -101,17 +104,17 @@ int main(int argc, char** argv) {
         
         if (orden == ORDEN_APAGADO) {
             ss << "El dispositivo " << id << " recibe la orden de apagado (" << orden << "). Byebye!";
-            Logger::notice(ss.str().c_str(), __FILE__);
+            Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
             ss.str("");
             break;
         } else if (orden == ORDEN_REINICIO){
             ss << "El dispositivo " << id << " recibe la orden de reinicio (" << orden << "). Vuelvo pronto!";
-            Logger::notice(ss.str().c_str(), __FILE__);
+            Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
             ss.str("");
             break;
         }else{
             ss << "El dispositivo " << id << " recibe la orden de hacer testeos especiales!";
-            Logger::notice(ss.str().c_str(), __FILE__);
+            Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
             ss.str("");
 
             bool ultimoTestEspecial = false;
@@ -119,7 +122,7 @@ int main(int argc, char** argv) {
             while (!ultimoTestEspecial){                
                 ss << "El dispositivo " << id << " recibe el programa especial numero " << program << ". Ejecutando test especial...";
                 correrTestEspecial( planillaVariables );
-                Logger::debug(ss.str().c_str(), __FILE__);
+                Logger::debug(ss.str().c_str(), nombreProceso.str().c_str());
                 ss.str("");
                 atendedor.enviarResultadoEspecial(id, rand() % 2);
                 program = atendedor.recibirProgramaEspecial(id);                
@@ -130,29 +133,29 @@ int main(int argc, char** argv) {
 
             if (orden == ORDEN_APAGADO) {
                 ss << "El dispositivo " << id << " recibe la orden de apagado (" << orden << "). Byebye!";
-                Logger::notice(ss.str().c_str(), __FILE__);
+                Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
                 ss.str("");
                 break;
             } else if (orden == ORDEN_REINICIO){
                 ss << "El dispositivo " << id << " recibe la orden de reinicio (" << orden << "). Vuelvo pronto!";
-                Logger::notice(ss.str().c_str(), __FILE__);
+                Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
                 ss.str("");
                 break;
             } else {
                 ss << "El dispositivo " << id << " recibe orden final desconocida!!";
-                Logger::notice(ss.str().c_str(), __FILE__);
+                Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
                 ss.str("");
                 break;
             }
         }
     } catch(std::string exception) {
-        Logger::error("Error en el dispositivo...", __FILE__);
+        Logger::error("Error en el dispositivo...", nombreProceso.str().c_str());
         break;
     } //Try-catch
     } //While
    
     ss << "El dispositivo " << id << " ha terminado el testeo"; 
-    Logger::notice(ss.str().c_str(), __FILE__);
+    Logger::notice(ss.str().c_str(), nombreProceso.str().c_str());
     
     Logger::destroy();
     
